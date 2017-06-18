0a0c9ca957319792701a0c29a3f60ce465b0b5bd
Anuradha Uduwage <uduw0001@umn.edu>
2017-06-17 21:54:03 -0400

    update database schema to introduce language and exepriment create time

--- ngs2-user-router/server/conf/evolutions/default/1.sql
+++ ngs2-user-router/server/conf/evolutions/default/1.sql
@@ -13,6 +13,7 @@ CREATE TABLE experiment (
     experiment_url varchar(255) NOT NULL,
     n_participants int(5) NOT NULL,
     status varchar(255) NOT NULL,
+    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
 
@@ -20,6 +21,7 @@ CREATE TABLE user_info (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   gallup_id bigint(20) NOT NULL,
   email varchar(255) NOT NULL,
+  language varchat(255) NOT NULL,
   randomized_id bigint(20) NOT NULL,
   arrival_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
@@ -27,14 +29,17 @@ CREATE TABLE user_info (
 
 CREATE TABLE experiment_user_info (
   id bigint(20) NOT NULL AUTO_INCREMENT,
-  FOREIGN KEY (user_id) REFERENCES `user`(id),
+  user_info_id bigint(20) NOT NULL,
+  FOREIGN KEY (user_info_id) REFERENCES user_info(id),
+  experiment_id bigint(20) NOT NULL,
   FOREIGN KEY (experiment_id) REFERENCES experiment(id),
   arrival_time TIMESTAMP NOT NULL,
-  send_off_time TIMESTAMP NOT NULL
+  send_off_time TIMESTAMP NOT NULL,
+  PRIMARY KEY (id)
 );
 
 # --- !Downs
 DROP TABLE experiment;
 DROP TABLE user_info;
 DROP TABLE experiment_user_info;
-
+DROP TABLE admin
