0d98ddd0a4ce7c3583f34cad08f95c2bf35bc407
Anuradha Uduwage <uduw0001@umn.edu>
2017-06-21 11:36:29 -0400

    Add ebean support and implement SHA-512 hasing for the password

--- ngs2-user-router/server/app/models/Admin.java
+++ ngs2-user-router/server/app/models/Admin.java
@@ -1,7 +1,13 @@
 package models;
 
 import com.avaje.ebean.Model;
+import play.data.validation.Constraints;
+
 import javax.persistence.*;
+import javax.validation.Constraint;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by anuradha_uduwage on 6/7/17.
@@ -12,9 +18,43 @@ public class Admin {
     @Id
     public Long id;
 
-    @Constrains.Required
+    @Column(length = 255, unique = true, nullable = false)
+    @Constraints.MaxLength(255)
+    @Constraints.Required
+    @Constraints.Email
     public String username;
-    public String password;
+
+    @Column(length = 64, nullable = false)
+    private byte[] shaPassword;
+
+    /**
+     * Set email address as the username.
+     * @param username email address as the username
+     */
+    public void setUsername(String username) {
+        this.username = username.toLowerCase();
+    }
+
+    /**
+     * Take user password and set it as SHA-512 has value of the password.
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.shaPassword = getShar512(password);
+    }
+
+
+    public static byte[] getShar512(String value) {
+        try {
+            return MessageDigest.getInstance("SHA-512").digest(value.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex);
+        } catch (UnsupportedEncodingException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+
 
 
 }
