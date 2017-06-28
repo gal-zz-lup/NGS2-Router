bfa58bc1bb263c938b53a9c8f93f1ac6df259f66
Anuradha Uduwage <uduw0001@umn.edu>
2017-06-28 12:15:17 -0400

    adding javascript to handle client side of the admin login #1

---
+++ ngs2-user-router/client/app/scripts/controllers/login.js
@@ -0,0 +1,42 @@
+/**
+ * Created by anuradha_uduwage on 6/23/17.
+ */
+angular.module('clientApp')
+  .controller('LoginCtrl', function ($scope, userService, $location, $log, $http, alertService) {
+
+    $scope.isAuthenticated = function() {
+      if(userService.username) {
+        $log.debug(userService.username);
+        // where should user be landing
+      } else {
+        $http.get('/app/isauthenticated')
+          .error(function() {
+            $location.path('/login');
+          })
+          .success(function(data) {
+            if(data.hasOwnProperty('success')) {
+              userService.username = data.success.user;
+              // where should the user be landing CRUD service?
+            }
+          });
+      }
+    };
+
+    $scope.isAuthenticated();
+
+    $scope.login = function() {
+
+      var payload = {
+        email : this.email,
+        password : this.password
+      };
+
+      $http.post('/app/login', payload)
+        .error(function(data){
+          log.debug(data)
+        })
+        .success(function(data){
+          $log.debug(data);
+        });
+    };
+  });
