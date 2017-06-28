1bf755f1df380668056a27e9e373bab5d92adb3c
Anuradha Uduwage <uduw0001@umn.edu>
2017-06-28 16:14:42 -0400

    adding login controller #1

--- ngs2-user-router/server/app/controllers/ApplicationController.java
+++ ngs2-user-router/server/app/controllers/ApplicationController.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import play.data.Form;
+import play.data.FormFactory;
+import play.data.validation.Constraints;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import javax.inject.Inject;
 
 
 /**
@@ -9,7 +14,35 @@ import play.mvc.Result;
  */
 public class ApplicationController extends Controller {
 
-    public static Result login() {
+    /**
+     * An action that renders login credentials to identify successful
+     * login action.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    @Inject
+    FormFactory formFactory;
+
+    public Result login() {
+        //final Form<Login> loginForm = formFactory.form(Login.class).bindFromRequest();
         return ok("Success!!!");
     }
+
+
+    /**x
+     * Static class for the user credential.
+     */
+    public static class UserForm {
+        @Constraints.Required
+        @Constraints.Email
+        public String email;
+    }
+
+    public static class Login extends UserForm {
+        @Constraints.Required
+        public String password;
+    }
+
+
 }
