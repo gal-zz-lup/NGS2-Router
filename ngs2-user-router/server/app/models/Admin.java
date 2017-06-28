633574ab7c62a95c043576e8f348338bb100ae6e
Anuradha Uduwage <uduw0001@umn.edu>
2017-06-28 11:59:13 -0400

    adding setters in admin #1

--- ngs2-user-router/server/app/models/Admin.java
+++ ngs2-user-router/server/app/models/Admin.java
@@ -5,13 +5,10 @@ import com.avaje.ebean.Model;
 import play.data.validation.Constraints;
 
 import javax.persistence.*;
-import javax.validation.Constraint;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+
 
 /**
- * Created by anuradha_uduwage on 6/7/17.
+ * Created by anuradha_uduwage.
  */
 @Entity
 public class Admin extends Model {
@@ -24,50 +21,29 @@ public class Admin extends Model {
     @Constraints.Required
     @Constraints.Email
     public String email;
+    @Constraints.Required
+    public String password;
 
-    @Column(length = 64, nullable = false)
-    private byte[] shaPassword;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    /**
-     * Set email address as the username.
-     * @param email email address as the username
-     */
     public void setEmail(String email) {
-
-        this.email = email.toLowerCase();
+        this.email = email;
     }
 
-    /**
-     * Take user password and set it as SHA-512 has value of the password.
-     * @param password
-     */
     public void setPassword(String password) {
+        this.password = password;
+    }
 
-        this.shaPassword = getShar512(password);
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
 
-    public static final Finder<Long, Admin> find = new Finder<Long, Admin>(
-            Long.class, Admin.class);
-
-    public static Admin findByUsernameAndPassword(String email, String password) {
-        return find
-                .where()
-                .eq("email", email.toLowerCase())
-                .eq("shaPassword", getShar512(password))
-                .findUnique();
-    }
-
-
-
-    public static byte[] getShar512(String value) {
-        try {
-            return MessageDigest.getInstance("SHA-512").digest(value.getBytes("UTF-8"));
-        } catch (NoSuchAlgorithmException ex) {
-            throw new RuntimeException(ex);
-        } catch (UnsupportedEncodingException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
 
 }
