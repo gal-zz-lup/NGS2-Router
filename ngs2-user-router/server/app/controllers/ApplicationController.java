81d59c9562d96ba38e550be1145b8d3507f209eb
Anuradha Uduwage <uduw0001@umn.edu>
2017-06-29 13:03:08 -0400

    adding file upload support

--- ngs2-user-router/server/app/controllers/ApplicationController.java
+++ ngs2-user-router/server/app/controllers/ApplicationController.java
@@ -1,12 +1,14 @@
 package controllers;
 
-import play.data.Form;
 import play.data.FormFactory;
 import play.data.validation.Constraints;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
 
 import javax.inject.Inject;
+import java.io.File;
 
 
 /**
@@ -27,6 +29,23 @@ public class ApplicationController extends Controller {
     public Result login() {
         //final Form<Login> loginForm = formFactory.form(Login.class).bindFromRequest();
         return ok("Success!!!");
+    }
+
+    public Result uploadCSVFile() {
+        try {
+            MultipartFormData<File> body = request().body().asMultipartFormData();
+            FilePart<File> csvFile = body.getFile("csvfile");
+
+            if (csvFile != null) {
+                String filename = csvFile.getFilename();
+                String contentType = csvFile.getContentType();
+                File file = csvFile.getFile();
+                //need to parse the file and convert to json
+                return ok("Success!!!");
+            }
+        } catch (Exception ex) {
+            return internalServerError();
+        }
     }
 
 
