5b2705b04f8d3422724e12ddf70c03dd0172dec6
Anuradha Uduwage <uduw0001@umn.edu>
2017-07-07 16:52:44 -0400

    building up experiment controller #4

--- ngs2-user-router/server/app/controllers/ExperimentController.java
+++ ngs2-user-router/server/app/controllers/ExperimentController.java
@@ -3,15 +3,25 @@ package controllers;
 import com.avaje.ebean.Model.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import models.Experiment;
+import play.data.Form;
+import play.data.FormFactory;
+import play.data.validation.Constraints;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import util.Utility;
 
+import javax.inject.Inject;
+import java.sql.Timestamp;
+
+
 /**
  * Created by anuradha_uduwage.
  */
 public class ExperimentController extends Controller {
+
+    @Inject
+    FormFactory formFactory;
 
     public Result getAllExperiments() {
         return ok(Json.toJson(Experiment.find.all()));
@@ -22,8 +32,14 @@ public class ExperimentController extends Controller {
         if (json == null) {
             return badRequest(Utility.createResponse("Expecting json", false));
         }
+
+        Form<ExperimentForm> experimentForm = formFactory.form(ExperimentForm.class).bindFromRequest();
         Experiment experiment = new Experiment();
-        // set all the fields
+        experiment.setActualURL(experimentForm.get().experimentName);
+        experiment.setShortenURL(experiment.getActualURL()); //need to implement url shortner
+        experiment.setCreatedTime(new Timestamp(System.currentTimeMillis()));
+        experiment.setNumberOfParticipants(experimentForm.get().numberOfParticipants);
+        experiment.setStatus(experimentForm.get().status);
         JsonNode jsonObject = Json.toJson(experiment);
         return created(Utility.createResponse(jsonObject, true));
     }
@@ -35,4 +51,28 @@ public class ExperimentController extends Controller {
     public Result deleteExperiment() {
         return ok("TODO");
     }
+
+    public static class ExperimentForm {
+
+        @Constraints.Required
+        @Constraints.MaxLength(255)
+        public String experimentName;
+
+        @Constraints.MaxLength(255)
+        @Constraints.Required
+        public String actualURL;
+
+        @Constraints.MaxLength(255)
+        @Constraints.Required
+        public String shortenURL;
+
+        @Constraints.Required
+        public int numberOfParticipants;
+
+        @Constraints.Required
+        public String status;
+
+    }
+
+
 }
