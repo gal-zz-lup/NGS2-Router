9131359ec4daf156b98abc1a0828cd6a7512fc91
Anuradha Uduwage <uduw0001@umn.edu>
2017-07-10 10:08:35 -0400

    Seperate out URL shortener logic #9

---
+++ ngs2-user-router/server/app/services/URLShortenerService.java
@@ -0,0 +1,18 @@
+package services;
+
+/**
+ * Created by anuradha_uduwage.
+ */
+public class URLShortenerService {
+
+    private String url;
+
+    public URLShortenerService(String url) {
+        this.url = url;
+        // need to implement url shortener logic
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
