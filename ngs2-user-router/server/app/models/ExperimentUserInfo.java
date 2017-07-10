dd76635a4719dcdead2f3b10d810b85d86ea2e3a
Anuradha Uduwage <uduw0001@umn.edu>
2017-07-10 15:39:32 -0400

    adding User and Experiment find methods #8

--- ngs2-user-router/server/app/models/ExperimentUserInfo.java
+++ ngs2-user-router/server/app/models/ExperimentUserInfo.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 
 
 import java.sql.Timestamp;
+import java.util.List;
 
 /**
  * Created by anuradha_uduwage.
@@ -19,10 +20,10 @@ public class ExperimentUserInfo extends Model {
     public Long userExperimentId;
 
     @ManyToOne
-    public UserInfo userId;
+    public UserInfo user;
 
     @ManyToOne
-    public Experiment experimentId;
+    public Experiment experiment;
 
     @Constraints.Required
     @Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
@@ -32,4 +33,55 @@ public class ExperimentUserInfo extends Model {
     @Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
     public Timestamp sendOffTime;
 
+    public static Finder<Long, ExperimentUserInfo>
+            find = new Finder<Long, ExperimentUserInfo>(ExperimentUserInfo.class);
+
+    public static List<Experiment> findByExperiment(Experiment experiment) {
+        return Experiment.find.where().eq("experiment", experiment).findList();
+    }
+
+    public static List<UserInfo> findByUser(UserInfo userInfo) {
+        return UserInfo.find.where().eq("userInfo", userInfo).findList();
+    }
+
+
+    public Long getUserExperimentId() {
+        return userExperimentId;
+    }
+
+    public void setUserExperimentId(Long userExperimentId) {
+        this.userExperimentId = userExperimentId;
+    }
+
+    public UserInfo getUser() {
+        return user;
+    }
+
+    public void setUserId(UserInfo user) {
+        this.user = user;
+    }
+
+    public Experiment getExperiment() {
+        return experiment;
+    }
+
+    public void setExperiment(Experiment experiment) {
+        this.experiment = experiment;
+    }
+
+    public Timestamp getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Timestamp arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public Timestamp getSendOffTime() {
+        return sendOffTime;
+    }
+
+    public void setSendOffTime(Timestamp sendOffTime) {
+        this.sendOffTime = sendOffTime;
+    }
 }
