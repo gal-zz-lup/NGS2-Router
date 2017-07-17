66058d94e3af9171e6be0cad03ddd7c55b7fb012
Anuradha Uduwage <uduw0001@umn.edu>
2017-07-16 22:15:47 -0400

    implement csv parser and file upload logic #12

--- ngs2-user-router/server/app/controllers/ApplicationController.java
+++ ngs2-user-router/server/app/controllers/ApplicationController.java
@@ -1,15 +1,20 @@
 package controllers;
 
+import play.Logger;
 import play.data.FormFactory;
-import play.data.validation.Constraints;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
-import play.mvc.Security;
+import util.CSVFileReader;
+import util.Utility;
 
 import javax.inject.Inject;
 import java.io.File;
+import java.io.InputStream;
+
+import java.util.List;
 
 
 /**
@@ -33,17 +38,26 @@ public class ApplicationController extends Controller {
 
     public Result uploadCSVFile() {
         try {
+            List<Object> recordsList = null;
             MultipartFormData<File> body = request().body().asMultipartFormData();
             FilePart<File> csvFile = body.getFile("csvfile");
 
-            if (csvFile != null) {
+            if (csvFile != null && csvFile.getFilename().contains(".csv")) {
                 String filename = csvFile.getFilename();
-                String contentType = csvFile.getContentType();
-                File file = csvFile.getFile();
-                //need to parse the file and convert to json
+                CSVFileReader fileReader = CSVFileReader.getReaderInstance();
+                try {
+                    File file = csvFile.getFile();
+                    InputStream inputStream = fileReader.uploadFile(file);
+                    recordsList = fileReader.parseFile(inputStream);
+                } catch (Exception ex) {
+                    Logger.error("Exception occured::: " + ex.getMessage());
 
+                }
             }
-            return ok("File upload success!!!");
+            if (recordsList != null)
+                return ok(Json.toJson(recordsList));
+            else
+                return badRequest(Utility.createResponse("There were no records in the list", false));
         } catch (Exception ex) {
             return internalServerError();
         }
