07331c21a6a9b5869cbf2910c131d095ad7da90c
Anuradha Uduwage <uduw0001@umn.edu>
2017-08-03 08:09:05 -0400

    model changes csv file upload client and server side

--- ngs2-user-router/client/app/scripts/controllers/import-users.controller.js
+++ ngs2-user-router/client/app/scripts/controllers/import-users.controller.js
@@ -21,3 +21,22 @@
   ImportUsersController.$inject = ['$scope', '$uibModalInstance', 'UsersService'];
 
 })();
+
+/*
+Upload.upload({
+  url: '/api/upload',
+  method:'POST',
+  data: {
+    "file": user.profilePic, //file object
+
+  }
+}).then(function (resp) {
+  //console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+
+}, function (resp) {
+  console.log('Error status: ' + resp.status);
+}, function (evt) {
+  var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+  //console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+});
+*/
