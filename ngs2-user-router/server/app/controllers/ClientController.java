7888440567fc38e26cca6c3dac2f39694d4a6f1e
Mark McKnight <mark.mcknight@yale.edu>
2017-08-07 14:11:49 -0400

    Updated client update route to return a progress object and a src URL. Client javascript now polls the server every 3 seconds for updates. Added test code to show increasing progress and changing iframe URLs.

--- ngs2-user-router/server/app/controllers/ClientController.java
+++ ngs2-user-router/server/app/controllers/ClientController.java
@@ -1,14 +1,21 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import models.UserInfo;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.twirl.api.Html;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.Random;
 
 public class ClientController extends Controller {
+  private int valueTemp = 0;
+  private int TEMPMAXVALUE = 20;
+  private int TEMPMINVALUE = 0;
+
   public Result login(String clientId) {
     UserInfo user = UserInfo.find.where().eq("randomized_id", clientId).findUnique();
     if (user == null) {
@@ -22,5 +29,27 @@ public class ClientController extends Controller {
     user.save();
     Html clientTemplate = views.html.client.render(user);
     return ok(clientTemplate);
+  }
+
+  public Result update(String clientId) {
+    ObjectNode result = Json.newObject();
+    result.put("clientId", clientId);
+    // TODO: validate clientId efficiently before returning result
+    Random random = new Random();
+    switch (random.nextInt(3)) {
+      case 0: result.put("src", "http://brdbrd.net");
+      break;
+      case 1: result.put("src", "http://breadboard.yale.edu");
+      break;
+      case 2: result.put("src", "http://xkcd.com");
+      break;
+    }
+    ObjectNode progress = Json.newObject();
+    progress.put("valuemax", TEMPMAXVALUE);
+    progress.put("valuemin", TEMPMINVALUE);
+    if (valueTemp < TEMPMAXVALUE) valueTemp++;
+    progress.put("value", valueTemp);
+    result.put("progress", progress);
+    return ok(result);
   }
 }
