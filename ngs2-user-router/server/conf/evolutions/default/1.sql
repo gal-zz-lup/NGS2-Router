7ec905ee37f7aed94c51273e20baf7e287033497
Mark McKnight <mark.mcknight@yale.edu>
2017-08-11 16:21:41 -0400

    Created ExperimentInstance.assignUserInfo method for assigning users to experiment instances. Added current_game_url field to user_info table to persist the custom URL assigned by the system to the user.

--- ngs2-user-router/server/conf/evolutions/default/1.sql
+++ ngs2-user-router/server/conf/evolutions/default/1.sql
@@ -41,6 +41,7 @@ CREATE TABLE user_info (
   last_check_in TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   status VARCHAR(255) NOT NULL,
   sample_group VARCHAR(255) NOT NULL,
+  current_game_url VARCHAR(255),
   CONSTRAINT uq_user_info_randomized_id UNIQUE (randomized_id),
   CONSTRAINT pk_user_info PRIMARY KEY (id)
 );
